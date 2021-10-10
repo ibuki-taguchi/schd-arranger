8e1e18d59b0caa166e630d2abc0b4d49d6c06df4
ibuki <ibuki-taguchi@cybozu.co.jp>
2021-10-10 18:46:15 +0900

    first commit

--- app.js
+++ app.js
@@ -1,41 +1,120 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var helmet = require("helmet");
+var session = require("express-session");
+var passport = require("passport");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// モデルの読み込み
+var User = require("./models/user");
+var Schedule = require("./models/schd");
+var Availability = require("./models/avb");
+var Candidate = require("./models/cdd");
+var Comment = require("./models/cmt");
+User.sync().then(() => {
+  Schedule.belongsTo(User, { foreignKey: "createdBy" });
+  Schedule.sync();
+  Comment.belongsTo(User, { foreignKey: "userId" });
+  Comment.sync();
+  Availability.belongsTo(User, { foreignKey: "userId" });
+  Candidate.sync().then(() => {
+    Availability.belongsTo(Candidate, { foreignKey: "candidateId" });
+    Availability.sync();
+  });
+});
 
+var GitHubStrategy = require("passport-github2").Strategy;
+var GITHUB_CLIENT_ID = "25271351b9193735d78d";
+var GITHUB_CLIENT_SECRET = "313a92e7510d624e49772798895a6874b6ded222";
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      process.nextTick(function () {
+        User.upsert({
+          userId: profile.id,
+          username: profile.username,
+        }).then(() => {
+          done(null, profile);
+        });
+      });
+    }
+  )
+);
+
+var indexRouter = require("./routes/index");
+var loginRouter = require("./routes/login");
+var logoutRouter = require("./routes/logout");
 var app = express();
+app.use(helmet());
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(
+  session({
+    secret: "7fd10a1c0a952e49",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", indexRouter);
+app.use("/login", loginRouter);
+app.use("/logout", logoutRouter);
+
+app.get(
+  "/auth/github",
+  passport.authenticate("github", { scope: ["user:email"] }),
+  function (req, res) {}
+);
+
+app.get(
+  "/auth/github/callback",
+  passport.authenticate("github", { failureRedirect: "/login" }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
