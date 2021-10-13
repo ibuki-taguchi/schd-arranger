2a401febec9b40ab7bd5f3f29b550a8a98da69d1
ibuki <ibuki-taguchi@cybozu.co.jp>
2021-10-13 19:05:14 +0900

    #update CSRF対策、遷移元を記憶

--- app.js
+++ app.js
@@ -104,7 +104,14 @@ app.get(
   "/auth/github/callback",
   passport.authenticate("github", { failureRedirect: "/login" }),
   function (req, res) {
-    res.redirect("/");
+    var loginFrom = req.cookies.loginFrom;
+    // オープンリダイレクタ脆弱性対策
+    if (loginFrom && loginFrom.startsWith("/")) {
+      res.clearCookie("loginFrom");
+      res.redirect(loginFrom);
+    } else {
+      res.redirect("/");
+    }
   }
 );
 
